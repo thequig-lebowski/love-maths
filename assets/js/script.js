639b5bc926f6a4c53449977fa43b72183d3be866
David O'Quigley <oquigld2@gmail.com>
2024-07-28 13:45:17 +0000

    Add functionality to multiply game

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,6 +28,8 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -68,6 +70,8 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
+    } else if (operator === 'X') {
+        return [operand1 * operand2, "multiply"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting.`;
@@ -76,7 +80,7 @@ function calculateCorrectAnswer() {
 
 
 /**
- * Get the current score from the DOM and incrments it by 1
+ * Get the current score from the DOM and increments it by 1
  */
 function incrementScore() {
 
@@ -86,7 +90,7 @@ function incrementScore() {
 }
 
 /**
- * Gets the curren incorrect answer from the DOM and increments it by 1
+ * Gets the current incorrect answer from the DOM and increments it by 1
  */
 function incrementWrongAnswer() {
 
@@ -107,7 +111,10 @@ function displaySubtractionQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = 'X';
 
 }
 
