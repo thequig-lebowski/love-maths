87099789e1a18b00142f33e457585312867567f9
David O'Quigley <oquigld2@gmail.com>
2024-07-28 14:44:42 +0000

    Add code to division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
         })
     }
 
-    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+    document.getElementById("answer-box").addEventListener("keydown", function (event) {
         if (event.key === "Enter") {
             checkAnswers();
         }
@@ -36,6 +36,7 @@ function runGame(gameType) {
     //Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+    console.log(num1, num2);
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
@@ -43,7 +44,10 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractionQuestion(num1, num2);
-    } else {
+    } else if (gameType === "division") {
+        dispplayDivisionQuestion(num1, num2);
+    }
+    else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -87,7 +91,10 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === '-') {
         return [operand1 - operand2, "subtract"];
-    } else {
+    } else if (operator === '/') {
+        return [operand1 / operand2, "division"];
+    } 
+    else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting.`;
     }
@@ -135,6 +142,8 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 }
 
-function dispplayDivisionQuestion() {
-
+function dispplayDivisionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 * operand2;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '/';
 }
